6d2f832dbed3b929a27f3d7cb187f0124f86848f
Johan Ljungberg <jlj.9@hotmail.com>
2022-11-30 18:36:14 +0100

    Refactored MooGame.GenerateClue()

--- CleanCodeLaboration/MooGame.cs
+++ CleanCodeLaboration/MooGame.cs
@@ -54,28 +54,24 @@ public class MooGame
         return output;
     }
 
-    public static string GenerateClue(string goal, string guess)
+    public static string GenerateClue(string target, string guess)
     {
-        int cows = 0, bulls = 0;
-        guess += "    ";     // if player entered less than 4 chars
-        for (int i = 0; i < 4; i++)
+        string correct = "";
+        string close = "";
+        guess = guess.PadRight(target.Length);
+
+        for (int i = 0; i < target.Length; i++)
         {
-            for (int j = 0; j < 4; j++)
+            if (target.Contains(guess[i]))
             {
-                if (goal[i] == guess[j])
-                {
-                    if (i == j)
-                    {
-                        bulls++;
-                    }
-                    else
-                    {
-                        cows++;
-                    }
-                }
+                if (guess[i] == target[i])
+                    correct+= "B";
+                else
+                    close += "C";
             }
+
         }
-        return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
+        return $"{correct},{close}";
     }
 
     public static void showTopList()
