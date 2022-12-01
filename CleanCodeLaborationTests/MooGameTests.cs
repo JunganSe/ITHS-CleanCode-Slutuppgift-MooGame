8221e76f84cadc91aa40161adc983be6729d58db
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-01 10:57:33 +0100

    Added test for MooGame.StringifyPlayerData()

--- CleanCodeLaborationTests/MooGameTests.cs
+++ CleanCodeLaborationTests/MooGameTests.cs
@@ -55,4 +55,21 @@ public class MooGameTests
 
         Assert.AreEqual(expectedJson, actualJson);
     }
+
+    [TestMethod()]
+    public void StringifyPlayerDataTest()
+    {
+        // NOTE: Test breaks if variable "format" is changed in testee.
+
+        var testData = new List<PlayerData>();
+        testData.Add(new PlayerData("abc", 3));
+        testData.Add(new PlayerData("adasda", 3));
+        testData[0].AddGameEntry(8);
+        testData[1].AddGameEntry(4);
+        testData[0].AddGameEntry(7);
+
+        string expected = "Player   Games  Average\nabc          3     6,00\nadasda       2     3,50";
+        string actual = MooGame.StringifyPlayerData(testData);
+        Assert.AreEqual(expected, actual);
+    }
 }
