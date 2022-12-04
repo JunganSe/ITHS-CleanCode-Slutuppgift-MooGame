a38c9de782c473bf67e6c27611668094a6d17d74
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-04 12:25:13 +0100

    Created ui interface and concrete class

---
+++ CleanCodeLaboration/ConsoleUi.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanCodeLaboration;
+
+public class ConsoleUi : IUi
+{
+    public string GetUserInput()
+    {
+        return Console.ReadLine() ?? "";
+    }
+
+    public void PrintOutput(string output)
+    {
+        Console.WriteLine(output);
+    }
+}
---
+++ CleanCodeLaboration/IUi.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanCodeLaboration;
+
+public interface IUi
+{
+    public string GetUserInput();
+    public void PrintOutput(string output);
+}
