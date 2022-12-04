9f9f69088072ab712caeff365aba7e8550760bbd
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-04 15:24:35 +0100

    Added interface for games

---
+++ CleanCodeLaboration/IGame.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanCodeLaboration;
+
+public interface IGame
+{
+    public string GameName { get; init; }
+    public string ScoreFileName { get; init; }
+
+    public string GenerateTargetDigits();
+    public string GenerateClue(string target, string guess);
+}
