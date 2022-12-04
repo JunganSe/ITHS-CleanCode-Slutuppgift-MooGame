e5ed45f6cdbaa2bfb6b8f594ecc33e247c654c82
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-04 12:25:56 +0100

    Created base for game controller

---
+++ CleanCodeLaboration/GameController.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanCodeLaboration;
+
+public class GameController
+{
+    private IGame _game;
+    private IUi _ui;
+    public GameController(IGame game, IUi ui)
+    {
+        _game= game;
+        _ui= ui;
+    }
+
+    public void RunGame()
+    {
+
+    }
+}
