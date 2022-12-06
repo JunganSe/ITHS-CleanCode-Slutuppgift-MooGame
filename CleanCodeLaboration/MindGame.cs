d477b3135321ab368ed3d139bb7c7325743c8340
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-06 18:03:11 +0100

    Created MindGame

---
+++ CleanCodeLaboration/MindGame.cs
@@ -0,0 +1,25 @@
+﻿namespace CleanCodeLaboration;
+
+public class MindGame : IGame
+{
+    private readonly char _correctLetter;
+    private readonly char _closeLetter;
+
+    public string Name { get; init; }
+
+    public MindGame()
+    {
+        _correctLetter = 'B';
+        _closeLetter = 'C';
+        Name = "MindGame";
+    }
+
+    public string GenerateTargetDigits()
+    {
+        throw new NotImplementedException();
+    }
+    public string GenerateClue(string target, string guess)
+    {
+        throw new NotImplementedException();
+    }
+}
