a607135a5aba726c3eda858e7873e9299d9c6dc3
Johan Ljungberg <jlj.9@hotmail.com>
2022-12-11 11:33:08 +0100

    Refactored PlayerData to hold each game entry

--- CleanCodeLaboration/PlayerData.cs
+++ CleanCodeLaboration/PlayerData.cs
@@ -2,27 +2,23 @@
 
 public class PlayerData
 {
-    private int _totalGuessCount;
-
     public string Name { get; init; }
-    public int GamesCount { get; private set; }
-
+    public int GamesCount { get => GuessCounts.Count; }
+    public List<int> GuessCounts { get; private set; } = new();
 
     public PlayerData(string name, int guessCount)
     {
         Name = name;
-        GamesCount = 1;
-        _totalGuessCount = guessCount;
+        AddGameEntry(guessCount);
     }
 
     public void AddGameEntry(int guessCount)
     {
-        _totalGuessCount += guessCount;
-        GamesCount++;
+        GuessCounts.Add(guessCount);
     }
 
     public double GetAverageGuessCount()
     {
-        return (double)_totalGuessCount / GamesCount;
+        return GuessCounts.Sum() / (double)GamesCount;
     }
 }
